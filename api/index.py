bbd5c69577b1afe40bb306ed475e230facd62b41
ManelZy <124919778+ManelZy@users.noreply.github.com>
2024-02-02 22:59:32 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -210,8 +210,8 @@ def api_dishes_add():
         # Specify the desired storage path within the bucket
         storage_path = 'temp_image.png'
 
-        # Upload the image directly from bytes
-       response = supabase.storage.from_bucket(BUCKET_NAME).upload(file=dish_image, path=storage_path)
+        response = supabase.storage.from_bucket(BUCKET_NAME).upload(file=dish_image, path=storage_path)
+
 
         # Insert the new store
         response = supabase.table('dishes').insert({
