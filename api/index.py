36e15b9057301ea349d0dffcf32b420524153bef
ManelZy <124919778+ManelZy@users.noreply.github.com>
2024-02-03 00:58:31 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -533,11 +533,7 @@ def get_order_statistics(restaurant_id):
         order_count_response = supabase.table('orders').select('restaurant_id').eq('restaurant_id', restaurant_id).execute()
 
         # Retrieve the total price for the specified restaurant_id
-        total_price_response = supabase.table('orders')
-        .select('total_price')
-        .eq('restaurant_id', restaurant_id)
-        .in_('order_status', ['Accepted', 'Completed'])
-        .execute()
+        total_price_response = supabase.table('orders').select('total_price').eq('restaurant_id', restaurant_id).in_('order_status', ['Accepted', 'Completed']).execute()
 
         if order_count_response.data and total_price_response.data:
             order_count = len(order_count_response.data)
